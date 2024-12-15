0e1f4eaddd6c9a10ad44ae9608095447d5616eda
ajan9gallery <ajayvelicheti@my.unt.edu>
2024-12-15 13:28:02 -0600

    gather.tf

--- Jenkins-Server-TF/gather.tf
+++ Jenkins-Server-TF/gather.tf
@@ -8,3 +8,15 @@ data "aws_ami" "ami" {
 
   owners = ["099720109477"]
 }
+
+
+data "aws_ami" "ami" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-24.04-amd64-server-*"] 
+  }
+
+  owners = ["099720109477"]  # Canonical owner ID
+}
