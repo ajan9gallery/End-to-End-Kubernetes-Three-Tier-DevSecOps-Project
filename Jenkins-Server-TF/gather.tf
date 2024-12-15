1166f71c5774975ce7c0520a33b6d591eea41d7e
ajan9gallery <ajayvelicheti@my.unt.edu>
2024-12-15 14:01:05 -0600

    gather.tf

--- Jenkins-Server-TF/gather.tf
+++ Jenkins-Server-TF/gather.tf
@@ -7,4 +7,8 @@ data "aws_ami" "ami" {
   }
 
   owners = ["099720109477"]  # Canonical owner ID
+filter {
+    name   = "ami-id"
+    values = ["ami-0e2c8caa4b6378d8c"]  # Use the AMI ID you provided
+  }
 }
