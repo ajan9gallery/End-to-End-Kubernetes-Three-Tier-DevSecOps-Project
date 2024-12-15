f74d744c6aacb7dbecdeea1ced473316ababc53e
ajan9gallery <ajayvelicheti@my.unt.edu>
2024-12-15 13:35:41 -0600

    gather.tf

--- Jenkins-Server-TF/gather.tf
+++ Jenkins-Server-TF/gather.tf
@@ -3,18 +3,6 @@ data "aws_ami" "ami" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-
-  owners = ["099720109477"]
-}
-
-
-data "aws_ami" "ami" {
-  most_recent = true
-
-  filter {
-    name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-24.04-amd64-server-*"] 
   }
 
