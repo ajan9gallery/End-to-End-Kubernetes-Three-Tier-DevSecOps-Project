4fe70ab991f3b5877e5d121926a6b191e66a8159
ajan9gallery <ajayvelicheti@my.unt.edu>
2024-12-15 12:24:56 -0600

    backend.tf

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "kubtest1"
     region         = "us-east-1"
-    key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "kubtestdb1"
     encrypt        = true
   }
   required_version = ">=0.13.0"
